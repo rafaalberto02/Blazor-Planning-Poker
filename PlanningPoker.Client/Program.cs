2888a7ee8df5085e5a9609f6f895089336d88422
rafaalberto02 <rafa.alberto02@gmail.com>
2023-01-15 00:51:51 -0300

    Add: Use Of Static css

--- PlanningPoker.Client/Program.cs
+++ PlanningPoker.Client/Program.cs
@@ -1,7 +1,6 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseStaticWebAssets();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
@@ -9,7 +8,6 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
