9e05002fa8319be062d792aaae460d13ae96aeac
rafaalberto02 <rafa.alberto02@gmail.com>
2023-01-19 00:37:52 -0300

    Add: Redirect to Room page sending RoomId

--- PlanningPoker.Client/Pages/RoomsList/Components/RoomsListItem.razor.cs
+++ PlanningPoker.Client/Pages/RoomsList/Components/RoomsListItem.razor.cs
@@ -5,5 +5,12 @@ namespace PlanningPoker.Client.Pages.RoomsList.Components;
 
 public partial class RoomsListItem
 {
+    [Inject] public NavigationManager NavigationManager { get; set; } = default!;
     [Parameter] public RoomDto? RoomObject { get; set; }
+
+    private void GotoRoomPage()
+    {
+        if (RoomObject is not null)
+            NavigationManager.NavigateTo($"/Room/{RoomObject.Id}");
+    }
 }
