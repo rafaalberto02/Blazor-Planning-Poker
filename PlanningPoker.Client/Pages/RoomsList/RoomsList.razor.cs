0f1e572a109b172e8d878b30ab4dffdcd586ce62
rafaalberto02 <rafa.alberto02@gmail.com>
2023-01-18 23:47:53 -0300

    wip: Moved code from Index to RoomsList

--- PlanningPoker.Client/Pages/RoomsList/RoomsList.razor.cs
+++ PlanningPoker.Client/Pages/RoomsList/RoomsList.razor.cs
@@ -1,6 +1,20 @@
+using PlanningPoker.Client.Services.Rooms;
+using PlanningPoker.Shared.Dtos;
+
 namespace PlanningPoker.Client.Pages.RoomsList;
 
 public partial class RoomsList
 {
-    
+    private IReadOnlyCollection<RoomDto>? _roomCollection = null;
+    private readonly IRoomService _roomService;
+
+    public RoomsList(IRoomService roomService)
+    {
+        _roomService = roomService;
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _roomCollection = await _roomService.GetActiveRooms();
+    }
 }
