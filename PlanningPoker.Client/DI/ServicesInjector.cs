c8b0c8ee6d0dcbf43b1a29079889d7e89563fba8
rafaalberto02 <rafa.alberto02@gmail.com>
2023-01-18 23:09:25 -0300

    Remove: Force non-nullable

--- PlanningPoker.Client/DI/ServicesInjector.cs
+++ PlanningPoker.Client/DI/ServicesInjector.cs
@@ -6,9 +6,9 @@ public static class ServicesInjector
 {
     public static void AddServices(this WebApplicationBuilder builder)
     {
-        var baseAddress = builder.Configuration["PlanningPokerServerUrl"];
+        var baseAddress = builder.Configuration["PlanningPokerServerUrl"] ?? string.Empty;
 
         var services = builder.Services;
-        services.AddHttpClient<IRoomService, RoomService>(client => client.BaseAddress = new Uri(baseAddress!));
+        services.AddHttpClient<IRoomService, RoomService>(client => client.BaseAddress = new Uri(baseAddress));
     }
 }
