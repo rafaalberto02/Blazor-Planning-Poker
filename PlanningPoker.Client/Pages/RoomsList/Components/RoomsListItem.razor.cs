ca19a604cfe317f9d4d59da51aecae1f24089188
rafaalberto02 <rafa.alberto02@gmail.com>
2023-01-19 00:08:08 -0300

    Refactoring: Moved content from RoomSelectorItem to RoomsListItem, wich makes more sense

--- PlanningPoker.Client/Pages/RoomsList/Components/RoomsListItem.razor.cs
+++ PlanningPoker.Client/Pages/RoomsList/Components/RoomsListItem.razor.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Components;
+using PlanningPoker.Shared.Dtos;
+
 namespace PlanningPoker.Client.Pages.RoomsList.Components;
 
 public partial class RoomsListItem
 {
-    
+    [Parameter] public RoomDto? RoomObject { get; set; }
 }
