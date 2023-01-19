bdf3f37bc674771d2ffc5440ef7062ee1ccc67f2
rafaalberto02 <rafa.alberto02@gmail.com>
2023-01-18 23:29:26 -0300

    Add: RoomObject to RoomSelectorItem using RoomDto

--- PlanningPoker.Client/Shared/Components/RoomSelectorItem.razor.cs
+++ PlanningPoker.Client/Shared/Components/RoomSelectorItem.razor.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Components;
+using PlanningPoker.Shared.Dtos;
+
 namespace PlanningPoker.Client.Shared.Components;
 
 public partial class RoomSelectorItem
 {
+    [Parameter]
+    public RoomDto? RoomObject { get; set; }
+    
     
 }
